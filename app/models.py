79311638962c52c2131c6081c21f3cb9d6faf3aa
kadeeja <kadeejagriffin@yahoo.com>
2024-01-16 23:42:50 -0600

    Users can now add/delete task. Also need authentication

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
 from app import db
-from datetime import datetime
+import base64
+import os
+import re
+from datetime import datetime, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
@@ -10,6 +13,9 @@ class User(db.Model):
     username = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    #tasks = db.relationship('Task', backref='author')
+    token = db.Column(db.String(32), index = True, unique=True)
+    token_expiration = db.Column(db.DateTime)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -17,9 +23,28 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.id}|{self.username}>"
+    
+    def update(self,**kwargs):
+        allowed_fields = {'first_name', 'last_name', 'email', 'username', 'password'}
+        
+        def camel_to_snake(string):
+            return re.sub("([A-Z][A-Za-z]*)","_\1", string).lower()
+        
+        for key, value in kwargs.items():
+            snake_key = camel_to_snake(key)
+            if snake_key in allowed_fields:
+                if snake_key == 'password':
+                    self.set_password(value)
+                else:
+                    setattr(self, snake_key, value)
+        self.save()
 
     def save(self):
         db.session.add(self)
+        db.session.commit()
+        
+    def delete(self):
+        db.session.delete(self)
         db.session.commit()
 
     def set_password(self, password):
@@ -38,15 +63,14 @@ class User(db.Model):
             "email": self.email
         }
         
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "firstName": self.first_name,
-            "lastName": self.last_name,
-            "username": self.username,
-            "email": self.email
-        }
-        
+    def get_token(self):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(minutes=1):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode("utf-8") 
+        self.token_expiration = now + timedelta(hours=1)
+        self.save()
+        return self.token
         
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -64,7 +88,22 @@ class Task(db.Model):
     def __repr__(self):
         return f"<Task {self.id}|{self.title}>"
     
+    def update(self, **kwargs):
+        allowed_fields = {"title", "description"}
+    
+        def camel_to_snake(string):
+            return re.sub("([A-Z][A-Za-z]*)","_\1", string).lower()
+        for key, value in kwargs.items():
+            snake_key = camel_to_snake(key)
+            if snake_key in allowed_fields:
+                setattr(self, snake_key, value)
+        self.save()
+    
     def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def delete(self):
         db.session.add(self)
         db.session.commit()
         
--- app/routes.py
+++ app/routes.py
@@ -3,16 +3,17 @@ from app import app, db
 from fake_data.tasks import tasks_list
 from datetime import datetime
 from app.models import User, Task
-
-@app.route('/')
-def index():
-    first_name = 'Kadeeja'
-    last_name = 'Griffin'
-    age = 27
-    return 'Hello World'
+from app.auth import basic_auth, token_auth
 
 # USER ENDPOINTS
-
+@app.route("/token")
+@basic_auth.login_required
+def get_token():
+    user = basic_auth.current_user()
+    token = user.get_token()
+    return {"token": token, 
+            "tokenExpiration":user.token_expiration}
+    
 # Create New User
 @app.route('/users', methods=['POST'])
 def create_user():
@@ -46,6 +47,56 @@ def create_user():
     new_user = User(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
     return new_user.to_dict(), 201
 
+    #update 
+@app.route('/users/<int:user_id>', methods=['POST'])
+@token_auth.login_required
+def edit_user(user_id):
+    #check if they sent the data correctly
+    if not request.is_json:
+        return {'error': 'Your content type must be application/json!'}, 400
+    # get user based off id
+    user = db.session.get(User, user_id)
+    # make sure it exists
+    if user is None:
+        return {'error': f"user with {user_id} does not exist"}, 404
+    # get their token 
+    current_user = token_auth.current_user()
+    # make sure they are the person logged in 
+    if user is not current_user:
+        return {'error': 'You cannot change this user as you are not them!'}, 403
+    # then we update! 
+    data = request.json
+    user.update(**data)
+    return user.to_dict()
+
+# delete
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+@token_auth.login_required
+def delete_user(user_id):
+    # get the user based on the id
+    user = db.session.get(User, user_id)
+    # get token
+    current_user = token_auth.current_user()
+    # make sure its a real user
+    if user is None:
+        return {'error': f"User with {user_id} not found!"}, 404
+    # make sure user to delete is current user
+    if user is not current_user:
+        return {'error': 'You cant do that, delete yourself only'}, 403
+    # delete user
+    user.delete()
+    return {'success': f"{user.username} has been deleted"}
+
+# retrieve
+@app.route("/users/<int:user_id>")
+def get_user(user_id):
+    #get the user
+    user = db.session.get(User, user_id)
+    #if no user let them know
+    if user:
+        return user.to_dict()
+    else:
+        return {'error': f"user with id:{user_id} not found"}, 404
 
 # TASKS ENDPOINT
 
@@ -53,7 +104,7 @@ def create_user():
 @app.route('/tasks')
 def get_tasks():
     # Get tasks from storage (fake data)
-    tasks = db.session.execute(db.select(Task)).scalars().all
+    tasks = db.session.execute(db.select(Task)).scalars().all()
     # return a list of the dictionary version of each task in tasks
     return [t.to_dict() for t in tasks]
 
@@ -69,11 +120,12 @@ def get_task(task_id):
     
 # create a new task
 @app.route('/tasks', methods=['POST'])
+@token_auth.login_required
 def create_task():
     # Check to see that the request body is JSON
     if not request.is_json:
         return {'error': " Your content-type must be application/json"}, 400
-    # Get data fro the request
+    # Get data from the request
     data = request.json
     # Check that the data has the required fields
     required_fields = ['title', 'description']
@@ -84,11 +136,55 @@ def create_task():
         if missing_fields:
             return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
         
-        # Get the values from the request data
-        title = data.get('title')
-        description = data.get('description')
+    # Get the values from the request data
+    title = data.get('title')
+    description = data.get('description')
+    
+    #get logged in user
+    user = token_auth.current_user()
         
-        # Create a new instance of Task which will add to our database
-        new_task = Task(title=title, description=description)
-        return new_task.to_dict(), 201
+    # Create a new instance of Task which will add to our database
+    new_task = Task(title=title, description=description)
+    return new_task.to_dict(), 201
+    
+#update
+# edit Task
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+@token_auth.login_required
+def edit_task(task_id):
+    # check if they sent a good request
+    if not request.is_json:
+        return {'error': "Your content-type is not application/json"}, 400
+    # lets find the post in our db
+    task = db.session.get(Task, task_id)
+    # if we cant find it, let em know
+    if task is None:
+        return {'error': f'task with the id of {task_id} does not exist'}, 404
+    # get the token from current user
+    current_user = token_auth.current_user()
+    # check if they are the og or they cant edit
+    if task.author is not current_user:
+        return {'error': 'This is not your task! Try again.'}, 403
+    # then they can get green light
+    data = request.json
+    task.update(**data)
+    return task.to_dict()
 
+    #delete
+@app.route("/tasks/<int:task_id>", methods=['DELETE'])
+@token_auth.login_required
+def delete_task(task_id):
+    #get the task
+    task = db.session.get(Task, task_id)
+    # check if it exists 
+    if task is None:
+        return {'error': f'We cannot locate tasks with the id of {task_id}'}, 404
+    # get the logged in user token
+    current_user = token_auth.current_user()
+    # check to make sure the logged in user is task author
+    if task is not current_user:
+        return {'error': 'You can not do that, this is not your task!'}, 403
+    # delete task
+    task.delete()
+    return{'success':f"{task.title} has been deleted!"}
+    
